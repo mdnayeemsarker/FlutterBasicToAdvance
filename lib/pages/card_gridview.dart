f4aa4369d8a4220af8bedce36cd641bbbeba5f8e
Md Nayeem Asha <nayeemasha703@gmail.com>
2023-03-10 04:07:05 +0600

    GridView with container

--- lib/pages/card_gridview.dart
+++ lib/pages/card_gridview.dart
@@ -10,15 +10,34 @@ class CardGridViewPage extends StatelessWidget {
         title: Text("Card & GridView"),
       ),
       body: Center(
-        child: Card(
-          elevation: 10,
-          shape: RoundedRectangleBorder(
-              // borderRadius: BorderRadius.all(Radius.circular(20))),
-              // borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30))),
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(30))),
-          child: Container(
-            height: 200,
-            width: 200,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: GridView.count(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: [
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.amber,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.blueGrey,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.pink,
+              )
+            ],
           ),
         ),
       ),
